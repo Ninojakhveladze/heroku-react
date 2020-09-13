68f8a2b235a92cc3697581e1e13960ccbb24a420
unknown <nino.jakhveladze15b@geolab.edu.ge>
2020-09-13 11:34:42 +0400

    Updates

---
+++ precache-manifest.4ec316849ea5d219740a6e443c305c09.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecf086ff2b77d2415a2066b455a1035",
+    "url": "/heroku-react/index.html"
+  },
+  {
+    "revision": "e39ccd30617088b86c5d",
+    "url": "/heroku-react/static/css/main.60575270.chunk.css"
+  },
+  {
+    "revision": "c0f9fa0782d3fd97f188",
+    "url": "/heroku-react/static/js/2.508a6755.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroku-react/static/js/2.508a6755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39ccd30617088b86c5d",
+    "url": "/heroku-react/static/js/main.41a6752e.chunk.js"
+  },
+  {
+    "revision": "ce30638e3a9c04ba9d53",
+    "url": "/heroku-react/static/js/runtime-main.26c8f3fb.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/heroku-react/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/heroku-react/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/heroku-react/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/heroku-react/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "ec161a74448ccf2c18beefe8cca2b44a",
+    "url": "/heroku-react/static/media/Linearicons-Free.ec161a74.svg"
+  },
+  {
+    "revision": "3433f4f36bd2b243f04b681e6dd94cce",
+    "url": "/heroku-react/static/media/MercedesGLE.3433f4f3.png"
+  },
+  {
+    "revision": "d2ed2b710478ec16ca7ba2c87edb53af",
+    "url": "/heroku-react/static/media/audiQ3.d2ed2b71.png"
+  },
+  {
+    "revision": "bfbb1e92f9afa4f474c24a6e211b8367",
+    "url": "/heroku-react/static/media/banner-bg.bfbb1e92.jpg"
+  },
+  {
+    "revision": "1a4c94f9631e0109d2b36ab88cbbb444",
+    "url": "/heroku-react/static/media/beqa.1a4c94f9.jpg"
+  },
+  {
+    "revision": "2ec80a29bbd068e483c8bece2697c414",
+    "url": "/heroku-react/static/media/bmw.2ec80a29.png"
+  },
+  {
+    "revision": "c425fda07225495ca25b3b89fcf92b00",
+    "url": "/heroku-react/static/media/common-banner.c425fda0.jpg"
+  },
+  {
+    "revision": "7267cfdcf44b69d7a8ba1c048bf77725",
+    "url": "/heroku-react/static/media/exclusive.7267cfdc.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/heroku-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/heroku-react/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/heroku-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/heroku-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/heroku-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fde3524e3244b5feb1b24e2156a3a907",
+    "url": "/heroku-react/static/media/ford-1.fde3524e.png"
+  },
+  {
+    "revision": "8b535991e8c7b2775a80bce0a74714c3",
+    "url": "/heroku-react/static/media/honda-1.8b535991.png"
+  },
+  {
+    "revision": "49cca62f3813eb6f3bae81b78d598b06",
+    "url": "/heroku-react/static/media/jeep.49cca62f.png"
+  },
+  {
+    "revision": "832bd6f79d46a6d5fcd3e51fbeab8395",
+    "url": "/heroku-react/static/media/mercedes-1.832bd6f7.jpg"
+  },
+  {
+    "revision": "77126ca6a3967eb41fd215e7de746d7b",
+    "url": "/heroku-react/static/media/nemo.77126ca6.jpg"
+  },
+  {
+    "revision": "d18dae7166ea0860ee5cb365263d74bf",
+    "url": "/heroku-react/static/media/nino.d18dae71.jpg"
+  },
+  {
+    "revision": "0d07a3adfbb2108b77263d4279b8cd20",
+    "url": "/heroku-react/static/media/porsche-1.0d07a3ad.jpg"
+  },
+  {
+    "revision": "53edf967e691607fe55628898a3872ff",
+    "url": "/heroku-react/static/media/porsche-2.53edf967.jpg"
+  },
+  {
+    "revision": "126d01404f0e14ea8c380baa4e8f3727",
+    "url": "/heroku-react/static/media/saba.126d0140.jpg"
+  },
+  {
+    "revision": "30526486332f54ce64a11dec749fc9d8",
+    "url": "/heroku-react/static/media/suzuki.30526486.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/heroku-react/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/heroku-react/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/heroku-react/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/heroku-react/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "1507321bc6a2409699141f5734cf048e",
+    "url": "/heroku-react/static/media/toyota-1.1507321b.jpg"
+  },
+  {
+    "revision": "b4540031ec7c6c6ed9ff095ec198e248",
+    "url": "/heroku-react/static/media/toyotaRAV4.b4540031.png"
+  }
+]);
